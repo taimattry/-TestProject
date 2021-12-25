1e035c3e4eb1525e9317122ae699b81b155f0a3f
taymaf <taymaf@LT-490-PTH-6626.pdf.com>
2021-12-26 00:42:42 +0200

    add new print statement

--- basic/src/basic/Basic.java
+++ basic/src/basic/Basic.java
@@ -5,7 +5,7 @@ public class Basic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("helloooooooo1");
-		//System.out.print("helloooooooo2");
+		System.out.print("helloooooooo2");
 
 	}
 
